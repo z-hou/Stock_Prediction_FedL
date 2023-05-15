982eb840b54be4601f83ada4149a3d819d86447e
z-hou <53463603+z-hou@users.noreply.github.com>
2023-05-15 14:15:28 +0800

    Add files via upload

--- FedL.py
+++ FedL.py
@@ -17,6 +17,7 @@ import copy
 
 
 stock_data = pd.read_excel('./yahoo_data.xlsx')
+samples_data = [60, 203, 246, 214, 267]
 
 def train(model, model_index, train_data, train_label, num_epochs):
         criterion = torch.nn.MSELoss()
@@ -29,7 +30,7 @@ def train(model, model_index, train_data, train_label, num_epochs):
         for i in range(num_epochs):
             train_prediction = model(train_data)
             loss = criterion(train_prediction, train_label)
-            print("local model {}".format(model_index), " | Epoch ", i, " | Train MSE: ", loss.item())
+            #print("local model {}".format(model_index), " | Epoch ", i, " | Train MSE: ", loss.item())
             optimiser.zero_grad()
             loss.backward()
             optimiser.step()
@@ -54,26 +55,14 @@ class EdgeSystem(object):
             self.send_weights()
             self.clients_work()
             self.aggregation()
-            self.evaluation()
+            #self.evaluation()
+        self.evaluation()
 
     def clients_work(self):
         for i in range(self.client_number):
+            print("train model {}".format(i+1))
             self.local_models[i] = train(self.local_models[i], i, self.train_data[i], self.train_label[i], 20)
 
-    '''
-    def train(self, model, train_data, train_label, num_epochs):
-        criterion = torch.nn.MSELoss()
-        optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
-        model.train()
-        for i in range(num_epochs):
-            train_prediction = model(train_data)
-            loss = criterion(train_prediction, train_label)
-            print("local model {}".format(j), " | Epoch ", i, " | MSE: ", loss.item())
-            optimiser.zero_grad()
-            loss.backward()
-            optimiser.step()
-        return model
-    '''
 
     def send_weights(self):
         params = {}
@@ -101,7 +90,7 @@ class EdgeSystem(object):
             with torch.no_grad():
                 for k, v in self.local_models[j].named_parameters():
                     #params[k] += v * (self.local_models[j].len / s)
-                    params[k] += v * 0.2
+                    params[k] += v * samples_data[j]/990
         with torch.no_grad():
             for k, v in self.global_model.named_parameters():
                 v.copy_(params[k])
@@ -123,53 +112,131 @@ class EdgeSystem(object):
         plt.xlabel('date')
         plt.ylabel('price')
         plt.legend()
-        plt.show()
+        plt.savefig('./global.png')
+        plt.close()
+
+        for i in range(self.client_number):
+            model_ = self.local_models[i].eval()
+            test_pred_local = model(test_data)
+            test_pred_local = test_pred_local.detach().numpy()
+            rme_value_local= math.sqrt(mean_squared_error(self.eval_datalabel[:,0], test_pred_local[:,0]))
+            print('RME value for client {}: {} RMSE'.format(i, rme_value_local))
+            plt.figure()
+            plt.plot(test_pred, color='b', label='predict_price')
+            plt.plot(test_label, color='g', label='groundTruch_price')
+            plt.xlabel('date')
+            plt.ylabel('price')
+            plt.legend()
+            plt.savefig('./client_{}.png'.format(i))
+            plt.close()
+
+
+        
 
 
 
-'''
-train(model, num_epochs, x_train, y_train_lstm, criterion, optimiser):
-    loss_list=[]
-    for i in range(num_epochs):
-        y_train_pred = model(x_train)
-        loss = criterion(y_train_pred, y_train_lstm)
-        print("Epoch ", i, "MSE: ", loss.item*())
-        loss_list.append(loss.item())
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
-    torch.save(model, "price_predictor_epo{}.pth".format(num_epochs))
-    return y_train_pred, loss_list
-'''
 
-def Niid_data(client_number, train_data, train_label):
-    sample_numbers = train_label.shape[0]
-    print("Check sample_numbers: ", sample_numbers)
+def divide_data(client_number, train_data, train_label, NIID=False):
 
-    Partitioned_data = np.split(train_data, client_number, axis=0)
-    Partitioned_label = np.split(train_label, client_number, axis=0)
+    if NIID == False:
+        sample_numbers = train_label.shape[0]
+        print("Check sample_numbers: ", sample_numbers)
+
+        Partitioned_data = np.split(train_data, client_number, axis=0)
+        Partitioned_label = np.split(train_label, client_number, axis=0)
     
-    #for item in all_data:
-    #    print(item.shape)
-    return Partitioned_data, Partitioned_label
+        for item in Partitioned_data:
+            print(item.shape)
+        return Partitioned_data, Partitioned_label
+
+    else:
+        print("Check train_data: ", train_data.shape)
+        print("Check train_label: ", train_label.shape)
+        #print("train label: ", train_label)
+        train_label_new = np.sort(train_label, axis=0) ##按列排序
+        print("Check train_label_new: ", train_label_new.shape)
+        sort_indx = np.argsort(train_label, axis=0)
+        #print("index after argsort: ", sort_indx)
+        train_data_new_list = []
+        for ind in sort_indx:
+            train_data_new_list.append(train_data[ind])
+        train_data_new = np.squeeze(np.array(train_data_new_list), axis=1)
+        print("Check train_data_new: ", train_data_new.shape)
+        #for item in np.around(train_label, decimals=4):
+        #    print(item)
+        ##add categories
+        categ = []
+        for item in train_label:
+            if item >= -10.0 and item < -0.8:
+                categ.append(0)
+            if item >= -0.8 and item < -0.6:
+                categ.append(1)
+            if item >= -0.6 and item < -0.4:
+                categ.append(2)
+            if item >= -0.4 and item < -0.2:
+                categ.append(3)
+            if item >= -0.2 and item < 0:
+                categ.append(4)
+            if item >= 0 and item < 0.2:
+                categ.append(5)
+            if item >= 0.2 and item < 0.4:
+                categ.append(6)
+            if item >= 0.4 and item < 0.6:
+                categ.append(7)
+            if item >= 0.6 and item < 0.8:
+                categ.append(8)
+            if item >= 0.8 and item <= 10.0:
+                categ.append(9)
+            #if item <= -1 or item > 1.0:
+            #    print("out of range: ", item, item.dtype)
+        print("0: ", categ.count(0))
+        print("1: ", categ.count(1))
+        print("2: ", categ.count(2))
+        print("3: ", categ.count(3))
+        print("4: ", categ.count(4))
+        print("5: ", categ.count(5))
+        print("6: ", categ.count(6))
+        print("7: ", categ.count(7))
+        print("8: ", categ.count(8))
+        print("9: ", categ.count(9))
+        print("categ len: ", len(categ))
+        print(categ)
+        slice_point1 = categ.count(0)+categ.count(1)+categ.count(2)
+        slice_point2 = categ.count(0)+categ.count(1)+categ.count(2)+categ.count(3)
+        slice_point3 = categ.count(0)+categ.count(1)+categ.count(2)+categ.count(3)+categ.count(4)
+        slice_point4 = categ.count(0)+categ.count(1)+categ.count(2)+categ.count(3)+categ.count(4)+categ.count(5)+categ.count(6)+categ.count(7)
+    
+        print(slice_point1," ", slice_point2," ", slice_point3," ", slice_point4)
+        train_data_new = np.split(train_data_new, [slice_point1, slice_point2, slice_point3, slice_point4], axis=0)
+        train_label_new = np.split(train_label_new, [slice_point1, slice_point2, slice_point3, slice_point4], axis=0)
+        
+        for item in train_data_new:
+            print(item.shape)
+        for ite in train_label_new:
+            print(ite.shape)
+
+        return train_data_new, train_label_new
+
 
 
 if __name__ == '__main__':
     train_data, train_label, test_data, test_label, scaler = data_preprocess('./yahoo_data.xlsx', seq_length=20)
-    Partitioned_data, Partitioned_label = Niid_data(5, train_data, train_label)
+    Partitioned_data, Partitioned_label = divide_data(5, train_data, train_label, NIID=True)
 
+
+    
     input_dim = 1
-    # 隐藏层特征的维度
+    # 
     hidden_dim = 32
-    # 循环的layers
+    # LSTM layers
     num_layers = 2
-    # 预测后一天的收盘价
+    # prediction
     output_dim = 1
 
     model = build_model(input_dim, hidden_dim, num_layers, output_dim)
     EDsys = EdgeSystem(model, Partitioned_data, Partitioned_label, test_data, test_label, client_number=5, commu_round=25)
     EDsys.workflow()
-
+    
 
     #print(model.len)
     #for k,v in model.named_parameters():
